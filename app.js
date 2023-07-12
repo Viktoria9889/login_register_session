eb90c53276dea9068633588c4ca93ee5957795bb
Viktoria9889 <Pavlyshyn_Viktoria@yahoo.com>
2023-07-12 11:32:17 +0200

    second

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 const express = require('express')
-const server  = express()
+const server = express()
 const mongoose = require('mongoose');
 const session = require('express-session')
-const MongoStore = require('connect-mongodb-session')(session)
-
-
+const MongoStore = require('connect-mongo')
 
 const homeRoutes = require('./routes/home')
 const loginRoutes = require('./routes/auth')
 
-
 const mongoUrl = 'mongodb://127.0.0.1:27017/login'
 
-
-const storeMongo = new MongoStore({
-    collection: 'sessions',
-    uri: mongoUrl
-})
-
-server.set('view engine','ejs')
-server.set('views', __dirname + '/views')
-server.use(express.static(__dirname + '/public'))
-server.use(express.urlencoded({ extended: true }))
 
 server.use(session({
     secret: 'cat',
     resave: false,
     saveUninitialized: false,
-    store: storeMongo
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://127.0.0.1:27017/login',
+        stringify: false,
+    })
 }))
 
 
+server.set('view engine', 'ejs')
+server.set('views', __dirname + '/views')
+server.use(express.static(__dirname + '/public'))
+server.use(express.urlencoded({ extended: true }))
 
 server.use('/', homeRoutes)
 server.use('/auth', loginRoutes)
--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,10 @@ const { Router } = require('express')
 const router = Router()
 const User = require('../models/user')
 
-
-
 router.get('/', (req, res) => {
-    res.render('login', {
-        title: 'Вхід',
-
-    })
+    res.render('login')
 })
 
-router.get('/', (req, res) => {
-    res.render('register', {
-        title: 'Вхід',
-
-    })
-})
 router.get('/logout', (req, res) => {
     //реалізація виходу з кабінету і очищення даних з бд
     req.session.destroy(() => {
@@ -62,9 +51,7 @@ router.post('/register', async (req, res) => {
         if (candidate) {
             res.redirect('/auth#registre')
         } else {
-            const user = new User({
-                email, name, password
-            })
+            const user = new User({ email, name, password })
             await user.save()
             res.redirect('/auth#login')
         }
