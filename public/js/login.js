0a852a05e99f386e4505819a50b1cdfb1e0ee65d
Viktoria9889 <Pavlyshyn_Viktoria@yahoo.com>
2023-07-16 11:35:20 +0200

    fifth

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,14 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+const signInBtn = document.querySelector('.signin-btn')
+const signUpBtn = document.querySelector('.signup-btn')
+const formBox = document.querySelector('.form-box')
+const wrapperLogin = document.querySelector('.wrapper_login')
+
+signUpBtn.addEventListener('click', () => {
+    formBox.classList.add('active')
+    wrapperLogin.classList.add('active')
+})
+
+signInBtn.addEventListener('click', () => {
+    formBox.classList.remove('active')
+    wrapperLogin.classList.remove('active')
+})
